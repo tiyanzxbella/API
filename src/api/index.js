419a0bb49edaa943c803006fab5ffc07d9c76cfd
Alif Nugraha <yemobyte@gmail.com>
2026-04-05 13:37:47 +0800

    Remove blueArchiveRandom route registration
    Removed blueArchiveRandom route registration from setupRoutes.

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,7 @@
 import { statsRoute, statsHandler } from './stats/routes.js'
-import { blueArchiveRandomRoute, blueArchiveRandomHandler } from './ba/routes.js'
 
 import { register } from '../utils/route.js'
 
 export const setupRoutes = (app) => {
     register(app, statsRoute, statsHandler)
-    register(app, blueArchiveRandomRoute, blueArchiveRandomHandler)
 }
